051eb1e103b09d511e80a1ad59bb54980c44668b
Naman2004singh <naman.2singh9@gmail.com>
2025-01-03 21:53:10 +0530

    added the basic layout for the contact page with setting its routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/mobile/contact/contact_mobile.dart';
 import 'package:portfolio/mobile/home/landing_page_mobile.dart';
+import 'package:portfolio/web/contact/contact_web.dart';
 import 'package:portfolio/web/home/landing_page_web.dart';
 
 class Routes {
@@ -17,6 +19,16 @@ class Routes {
                     }
                   },
                 ));
+          case "/contact":
+          return MaterialPageRoute(
+            settings: settings,
+            builder: (_) => LayoutBuilder(builder: (context, constraints) {
+              if (constraints.maxWidth > 800) {
+                return ContactWeb();
+              }else{
+                return ContactMobile();
+              }
+            },))
       default:
         return MaterialPageRoute(
             settings: settings,
